b3d72bdefa6bccf6d61280aa45d3a5eaa97533c5
romulocia <80111359+romulocia@users.noreply.github.com>
2021-05-11 20:47:57 -0300

    Inclusion of module 07

--- src/module07/TestaConta.java
+++ src/module07/TestaConta.java
@@ -4,5 +4,6 @@ public class TestaConta {
     public static void main(String[] args) {
         Conta c1 = new Conta(1, 1, "Banco 01", 10.0);
         System.out.println(c1);
+        System.out.println(c1);
     }
 }
