e2896d89bf9a205b1a00604cb31ed74bdda7c7fe
romulocia <80111359+romulocia@users.noreply.github.com>
2021-05-11 18:49:17 -0300

    Inclusion of module 07

---
+++ src/module07/Conta.java
@@ -0,0 +1,57 @@
+package module07;
+
+public class Conta {
+    private int numero;
+    private int agencia;
+    private String banco;
+    private double saldo;
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public void setAgencia(int agencia) {
+        this.agencia = agencia;
+    }
+
+    public String getBanco() {
+        return banco;
+    }
+
+    public void setBanco(String banco) {
+        this.banco = banco;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public Conta(int numero, int agencia, String banco, double saldo) {
+        this.numero = numero;
+        this.agencia = agencia;
+        this.banco = banco;
+        this.saldo = saldo;
+    }
+
+    @Override
+    public String toString() {
+        return "Conta{" +
+                "numero=" + numero +
+                ", agencia=" + agencia +
+                ", banco='" + banco + '\'' +
+                ", saldo=" + saldo +
+                '}';
+    }
+}
---
+++ src/module07/TestaConta.java
@@ -0,0 +1,8 @@
+package module07;
+
+public class TestaConta {
+    public static void main(String[] args) {
+        Conta c1 = new Conta(1, 1, "Banco 01", 10.0);
+        System.out.println(c1);
+    }
+}
